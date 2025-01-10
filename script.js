00230dd18989cc670f540a2eb9312fb713aa6e19
Sayali Shelar <sayalishelar1312@gmail.com>
2025-01-10 05:24:57 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Smooth Scroll Functionality
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
